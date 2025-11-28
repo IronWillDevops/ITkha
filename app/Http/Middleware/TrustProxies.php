fd970cd8f56ce36c640462dce10ecd4a9425dd82
IronWillDevops <380671617445r@gmail.com>
2025-11-28 12:48:16 +0000

    fix/real-client-ip

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,11 +1,9 @@
 <?php
 
 namespace App\Http\Middleware;
-
-use Closure;
-use Illuminate\Http\Request;
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\HttpFoundation\Request;
 
 class TrustProxies extends Middleware
 {
