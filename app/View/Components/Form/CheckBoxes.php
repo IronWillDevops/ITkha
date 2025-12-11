19a59628ebd7a650ddb3d82a835fabd73c515600
IronWillDevops <380671617445r@gmail.com>
2025-12-11 22:19:10 +0000

    Updated Changes

--- app/View/Components/Form/CheckBoxes.php
+++ app/View/Components/Form/CheckBoxes.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\View\Components\Admin\Form;
+namespace App\View\Components\Form;
 
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Checkboxes extends Component
+class CheckBoxes extends Component
 {
     /**
      * Create a new component instance.
@@ -40,6 +40,6 @@ class Checkboxes extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.admin.form.checkboxes');
+        return view('components.form.check-boxes');
     }
 }
