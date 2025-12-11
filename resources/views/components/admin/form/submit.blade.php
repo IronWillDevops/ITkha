26e0c275445c09eddad41f71e482a5ed283f9d15
IronWillDevops <380671617445r@gmail.com>
2025-12-11 00:30:47 +0000

    Refactor form components: replace admin form components with new submit component and update language files

--- resources/views/components/admin/form/submit.blade.php
+++
@@ -1,6 +0,0 @@
-<button 
-    type="submit" 
-    class="bg-primary text-text-primary-foreground hover:bg-primary/90  focus:ring focus:outline-none focus-visible:ring-ring  rounded-sm font-medium text-sm px-5 py-2.5 text-center {{ $class ?? '' }}"
->
-    {{ $label }}
-</button>
