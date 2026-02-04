a8323fdb0657510e6f88f101a9952ebcd37649bf
IronWillDevops <380671617445r@gmail.com>
2026-02-04 21:17:58 +0000

    Add email confirmation link lifetime control and improved 403 error message.

--- app/Http/Controllers/Public/Auth/Verify/VerifyEmailController.php
+++ app/Http/Controllers/Public/Auth/Verify/VerifyEmailController.php
@@ -13,8 +13,14 @@ class VerifyEmailController extends Controller
     public function __invoke(Request $request, $id, $hash)
     {
         $user = User::findOrFail($id);
+        //    TTL
+        $expires = $request->query('expires'); // timestamp из URL
+        
+        if ($expires && now()->timestamp > (int)$expires) {
+            dd("TTL");
+            abort(403, __('public/auth/verify.messages.invalid_or_expired_link'));
+        }
 
-        // Проверка подписи email
         if (!hash_equals((string) $hash, sha1($user->getEmailForVerification()))) {
             abort(403, __('public/auth/verify.messages.invalid_or_expired_link'));
         }
@@ -23,6 +29,7 @@ class VerifyEmailController extends Controller
 
             return redirect()->route('login')->with('success',  __('public/auth/verify.messages.email_already_verified'));
         }
+
         if ($user->status !== UserStatus::ACTIVE->value) {
             $user->status = UserStatus::ACTIVE->value;
             $user->save();
