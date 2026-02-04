280f7f5bc584ff21f99e104bdb844511942e3c20
IronWillDevops <380671617445r@gmail.com>
2026-02-04 19:56:11 +0000

    Update ReVerification\IndexController

--- app/Http/Controllers/Public/Auth/ReVerification/IndexController.php
+++ app/Http/Controllers/Public/Auth/ReVerification/IndexController.php
@@ -12,6 +12,6 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-         return z('public.auth.reverification.index');
+         return view('public.auth.reverification.index');
     }
 }
