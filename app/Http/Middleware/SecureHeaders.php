d786674787c9aa834bba1939ac48985d03364a69
IronWillDevops <380671617445r@gmail.com>
2026-02-28 13:26:36 +0000

    feat: Add SecureHeaders middleware for enhanced security headers.

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -15,7 +15,6 @@ class SecureHeaders
      */
     public function handle(Request $request, Closure $next): Response
     {
-        /** @var Response $response */
         $response = $next($request);
 
         // Базові заголовки (працюють для HTTP і HTTPS)
